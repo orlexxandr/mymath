fe04bed768dd64f755511853d23aa56ac3edaba4
orlexxandr <167635533+orlexxandr@users.noreply.github.com>
2024-06-27 21:19:53 +0300

    Create mymath.go

---
+++ mymath.go
@@ -0,0 +1,58 @@
+package mymath
+
+import "math"
+
+// Abs - возвращает абсолютное значение числа
+func Abs(x float64) float64 {
+    return math.Abs(x)
+}
+
+// Acos - возвращает арккосинус числа
+func Acos(x float64) float64 {
+    return math.Acos(x)
+}
+
+// Asin - возвращает арксинус числа
+func Asin(x float64) float64 {
+    return math.Asin(x)
+}
+
+// Atan - возвращает арктангенс числа
+func Atan(x float64) float64 {
+    return math.Atan(x)
+}
+
+// Ceil - возвращает наименьшее целое число, которое не меньше заданного числа
+func Ceil(x float64) float64 {
+    return math.Ceil(x)
+}
+
+// Cos - возвращает косинус числа
+func Cos(x float64) float64 {
+    return math.Cos(x)
+}
+
+// Exp - возвращает e в степени x
+func Exp(x float64) float64 {
+    return math.Exp(x)
+}
+
+// Floor - возвращает наибольшее целое число, которое не больше заданного числа
+func Floor(x float64) float64 {
+    return math.Floor(x)
+}
+
+// Log - возвращает натуральный логарифм числа
+func Log(x float64) float64 {
+    return math.Log(x)
+}
+
+// Sin - возвращает синус числа
+func Sin(x float64) float64 {
+    return math.Sin(x)
+}
+
+// Sqrt - возвращает квадратный корень из числа
+func Sqrt(x float64) float64 {
+    return math.Sqrt(x)
+}
